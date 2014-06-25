25d472e118fb5c79c86900dbccd60de6f08fd39a
aadecker91 <aadecker91@gmail.com>
2014-06-25 00:47:11 -0400

    Clean up
    Added some comments, checked for errors and cleaned up code a little bit

--- blackjack.cpp
+++ blackjack.cpp
@@ -30,8 +30,8 @@ int players_bank = 10000; 	// The player's total amount of money
 bool playerBlackjack;		// Used to indicate player Blackjack
 bool houseBlackjack;		// Used to indicate house Blackjack
 bool insurance;				// Used to indicate house upcard of an ACE
-bool playerDouble;			// Used to indicate a player doubling down
-bool playerDouble2;
+bool playerDouble;			// Flag used to indicate a player doubling down
+bool playerDouble2;			//  on a perticular hand
 bool playerDouble3;
 bool playerDouble4;
 bool playerSurrender;		// Used to indicate a player surrendering
@@ -60,14 +60,18 @@ void houseLogic(vector<card> &house_hand);
 void play(); // Plays a single perfect game for the player
 
 int main( int argc, char ** argv ) {
-	srand(time(NULL));
-	string command = "";
+	srand(time(NULL));		// Seeds the rand generator used to deal cards from the deck
+	string command = "";	// User input
 
+	// when the user input is anything but q, deal a game of blackjack and print
+	//  the results
 	while(command != "q") {
 		play();
 
+		// Prints the users bankroll after the last game of BJ
 		cout << players_bank << endl;
 
+		// Prints the players hand(s) after the game for the user to see
 		cout << "Your hand is: " << endl;
 		for(unsigned i = 0; i < players_hand.size(); i++){
 			cout << players_hand[i].r << " of " << players_hand[i].s << endl;
@@ -90,22 +94,25 @@ int main( int argc, char ** argv ) {
 				cout << players_hand4[i].r << " of " << players_hand4[i].s << endl;
 			}
 		}
+		// Prints the house hand for the user
 		cout << "The houses hand is:" << endl;
 		for(unsigned i = 0; i < house_hand.size(); i++){
 			cout << house_hand[i].r << " of " << house_hand[i].s << endl;
 		}
+		// Get next command from user
 		cin >> command;
 	}
 	return 0;
 }
 // Resets the deck and the house/player hands
 void shuffle(vector<card> &deck) {
-	// Erase both the player's and the house's hand
+	// Erase both the player's and the house's hand(s)
 	players_hand.erase(players_hand.begin(), players_hand.end());
 	players_hand2.erase(players_hand2.begin(), players_hand2.end());
 	players_hand3.erase(players_hand3.begin(), players_hand3.end());
 	players_hand4.erase(players_hand4.begin(), players_hand4.end());
 	house_hand.erase(house_hand.begin(), house_hand.end());
+	// Reshuffle the six decks
 	deck = {
 		{ 1, SPD, "Ace" }, { 2, SPD, "Two" }, { 3, SPD, "Three" }, { 4, SPD, "Four" }, { 5, SPD, "Five" }, { 6, SPD, "Six" }, { 7, SPD, "Seven" },
 		{ 8, SPD, "Eight" }, { 9, SPD, "Nine" }, { 10, SPD, "Ten" }, { 10, SPD, "Jack" }, { 10, SPD, "Queen" }, { 10, SPD, "King" },
@@ -160,7 +167,7 @@ void shuffle(vector<card> &deck) {
 
 void dealCards(vector<card> &deck, vector<card> &players_hand, vector<card> &house_hand) {
 	playerBlackjack = false;
-	houseBlackjack = false;		//Clear all the important flags
+	houseBlackjack = false;		//Clear all the important flags before the new hand is dealt
 	insurance = false;
 	playerDouble = false;
 	playerDouble2 = false;
@@ -185,18 +192,21 @@ void dealCards(vector<card> &deck, vector<card> &players_hand, vector<card> &hou
 	house_hand.push_back(deck[c]); 	//deal the second card to the house
 	deck.erase(deck.begin() + c); 	//remove dealt card from the deck
 
-	// Checks whether player and/or dealer has Blackjack
+	// Checks whether player has Blackjack and sets the appropriate flag
 	if (((players_hand[0].r == 10) && (players_hand[1].r == 1)) || ((players_hand[0].r == 1) && (players_hand[1].r == 10))) {
 		playerBlackjack = true;
 	}
-
+	// This is the dealers "peek," if the dealer has a ten showing he will peek for blackjack
+	// the insurance option is not included because you're never supposed to take insurance
+	// and the deck is shuffled after every hand so counting cards can't affct your
+	// decision to take insurance
 	if ((house_hand[0].r == 10) && (house_hand[1].r == 1)) {
 		houseBlackjack = true;
 	}
 }
 
 void hit(vector<card> &players_hand) {
-	firstHand = false;	// It's no longer the first hand
+	firstHand = false;					// It's no longer the first hand
 	int c; 								//random number used to deal a card from the deck
 	c = rand() % deck.size(); 			//generate random card number
 	players_hand.push_back(deck[c]); 	//deal first card to players hand
@@ -208,6 +218,7 @@ void doubleDown(vector<card> &players_hand) {
 	c = rand() % deck.size(); 			//generate random card number
 	players_hand.push_back(deck[c]); 	//deal first card to players hand
 	deck.erase(deck.begin() + c); 		//remove dealt card from the deck
+	// Set the flag for the deck the player doubled on
 	if (&players_hand == &players_hand2) {
 		playerDouble2 = true;
 	} else if (&players_hand == &players_hand3) {
@@ -221,21 +232,24 @@ void doubleDown(vector<card> &players_hand) {
 }
 
 void surrender(vector<card> &players_hand) {
-	playerSurrender = true; //Sets the player to surrender
+	playerSurrender = true; //Sets the flag for player to surrender
+			// the player can only surrender on the original hand that is dealt
 }
 
 void split(vector<card> &players_hand) {
-	card temp;
+	card temp;							// used to store one of the split cards
+	// Checks which hands are empty to ensure 1. the player cannot split to more than four hands and
+	// 2. The player's split will not overwrite/add to a previous split
 	if (players_hand2.empty()) {
-		temp = players_hand.back();
-		players_hand.pop_back();
-		players_hand2.push_back(temp);
-		hit(players_hand);
+		temp = players_hand.back();		// stores one of the players cards
+		players_hand.pop_back();		// removes that card
+		players_hand2.push_back(temp);	// pushes that card to another hand
+		hit(players_hand);				// hits both hands
 		hit(players_hand2);
-		firstHand = true;
-		playerLogic(players_hand);
-		firstHand = true;
-		playerLogic(players_hand2);
+		firstHand = true;				// set this flag to ensure that the player can double after a split
+		playerLogic(players_hand);		// follow player logic for the first new hand
+		firstHand = true;				// set this flag to ensure that the player can double after a split
+		playerLogic(players_hand2);		// follow player logic for the second new hand
 	} else if (players_hand3.empty()) {
 		temp = players_hand.back();
 		players_hand.pop_back();
@@ -245,9 +259,9 @@ void split(vector<card> &players_hand) {
 		firstHand = true;
 		playerLogic(players_hand);
 		firstHand = true;
-		playerLogic(players_hand3);
-	} else if (players_hand4.empty()) {
-		temp = players_hand.back();
+		playerLogic(players_hand3);		// The code for the other 2 possible cases
+	} else if (players_hand4.empty()) {	// this gives the player the opportunity to split a hand
+		temp = players_hand.back();		// up to four times
 		players_hand.pop_back();
 		players_hand4.push_back(temp);
 		hit(players_hand);
@@ -262,7 +276,7 @@ void split(vector<card> &players_hand) {
 
 // A function that plays one full game of Blackjack
 void play() {
-	// Initialize the player and house sums
+	// Initialize the player and house sums to be zero
 	unsigned int player_sum = 0;
 	unsigned int house_sum = 0;
 
@@ -271,14 +285,16 @@ void play() {
 
 	// Check for blackjack before play starts
 	if (!playerBlackjack && !houseBlackjack) {
-		playerLogic(players_hand); // Hit/stay for the player
-		houseLogic(house_hand); // Hit/stay for the house
+		playerLogic(players_hand); // Hit/stay/double/split logic for the player
+		houseLogic(house_hand); // Hit/stay logic for the house
 
+		// This is for a game where the player did not split his hand
 		if (players_hand2.empty()) {
-			// Calculate the player and house sums
+			// Calculate the player sum
 			for(unsigned int i = 0; i < players_hand.size(); i++) {
 				player_sum += players_hand[i].r;
 			}
+			// Calculates whether an ace will be treated as a one or eleven
 			for(unsigned int i = 0; i < players_hand.size(); i++) {
 				if (players_hand[i].r == 1) {
 					if ((player_sum + 10) < 22) {
@@ -287,9 +303,11 @@ void play() {
 				}
 			}
 
+			// Calculate the house sum
 			for(unsigned int i = 0; i < house_hand.size(); i++) {
 				house_sum += house_hand[i].r;
 			}
+			// Calculates whether an ace will be treated as a one or eleven
 			for(unsigned int i = 0; i < house_hand.size(); i++) {
 				if (house_hand[i].r == 1) {
 					if ((house_sum + 10) < 22) {
@@ -298,17 +316,19 @@ void play() {
 				}
 			}
 
+			// prints the players sum
 			cout << "The player had: ";
 			cout << player_sum << endl;
 
+			// prints the house sum
 			cout << "The dealer had: ";
 			cout << house_sum << endl;
 
 			// Win or Lose conditions
 			if (playerSurrender) {
 				cout << "YOU SURRENDERED" << endl;
-				players_bank -= players_bet/2;
-			} else if(playerDouble){
+				players_bank -= players_bet/2;		// player loses half his bet if he surrenders
+			} else if(playerDouble){		// players gains/loses doubles his bet if he doubles
 				if (player_sum > 21) {
 					cout << "YOU LOST THE DOUBLE" << endl;
 					players_bank -= (2 * players_bet);
@@ -341,7 +361,7 @@ void play() {
 					cout << "PUSH" << endl;
 				}
 			}
-		} else {
+		} else {		// the win/lose conditions when the player has multiple hands
 			player_sum = 0;
 			house_sum = 0;
 			// Calculate the player and house sums
@@ -408,6 +428,7 @@ void play() {
 				}
 			}
 
+			// the player's second hand sum and win/lose conditions
 			player_sum = 0;
 			house_sum = 0;
 			// Calculate the player and house sums
@@ -473,6 +494,9 @@ void play() {
 					cout << "PUSH" << endl;
 				}
 			}
+			// checks if the player had split to three hands and then checks
+			// the win/lose conditions and adds/subtracts money from the bank
+			// based on those conditions
 			if (!players_hand3.empty()) {
 				player_sum = 0;
 				house_sum = 0;
@@ -540,6 +564,8 @@ void play() {
 					}
 				}
 			}
+			// checks if the player split to four hands and then checks the win/loss
+			// conditions for the fourth hand and acts accordingly
 			if (!players_hand4.empty()) {
 				player_sum = 0;
 				house_sum = 0;
@@ -609,6 +635,8 @@ void play() {
 			}
 		}
 	} else if (playerBlackjack && !houseBlackjack) {
+		// what happens when the player has blackjack
+
 		// Calculate the player and house sum to print
 		for(unsigned int i = 0; i < players_hand.size(); i++) {
 			player_sum += players_hand[i].r;
@@ -641,6 +669,8 @@ void play() {
 		cout << "YOU GOT BLACKJACK" << endl;
 		players_bank += (players_bet + 5);
 	} else if (!playerBlackjack && houseBlackjack) {
+		// what happens when the house has blackjack
+
 		// Calculate player and house sum to print for debugging
 		for(unsigned int i = 0; i < players_hand.size(); i++) {
 			player_sum += players_hand[i].r;
@@ -673,6 +703,8 @@ void play() {
 		cout << "THE DEALER HAD BLACKJACK" << endl;
 		players_bank -= players_bet;
 	} else {
+		// what happens when both the player and the house have blackjack
+
 		// Calculate the player and house sum for printing
 		for(unsigned int i = 0; i < players_hand.size(); i++) {
 			player_sum += players_hand[i].r;
@@ -718,6 +750,7 @@ void playerLogic(vector<card> &players_hand) {
 	for(unsigned int i = 0; i < players_hand.size(); i++) {
 		player_sum += players_hand[i].r;
 	}
+	// used to distinguish when the player has a soft hand
 	for(unsigned int i = 0; i < players_hand.size(); i++) {
 		if (players_hand[i].r == 1) {
 			if ((player_sum + 10) < 22) {
@@ -730,7 +763,9 @@ void playerLogic(vector<card> &players_hand) {
 	house_sum = house_hand[0].r; 	// Calculating the house upcard which is just the
 									// first card of the house's hand
 
-	// Every player action based on the dealer upcard
+	// The following is logic for how the player should play when confronted with
+	// any dealer upcard, it is a series of embedded case/if statements
+	// the "hard" player hands are first
 	if (!playerSoftHand) {
 		switch(player_sum) {
 		case 4:
@@ -944,7 +979,6 @@ void playerLogic(vector<card> &players_hand) {
 				hit(players_hand);
 				playerLogic(players_hand);
 			}
-			break;
 			break;
 		case 9:
 			switch(house_sum) {
@@ -1194,25 +1228,16 @@ void playerLogic(vector<card> &players_hand) {
 				case 4:
 					if (players_hand4.empty()) {
 						split(players_hand);
-					} else {
-						hit(players_hand);
-						playerLogic(players_hand);
 					}
 					break;
 				case 5:
 					if (players_hand4.empty()) {
 						split(players_hand);
-					} else {
-						hit(players_hand);
-						playerLogic(players_hand);
 					}
 					break;
 				case 6:
 					if (players_hand4.empty()) {
 						split(players_hand);
-					} else {
-						hit(players_hand);
-						playerLogic(players_hand);
 					}
 					break;
 				case 7:
@@ -1321,41 +1346,26 @@ void playerLogic(vector<card> &players_hand) {
 				case 2:
 					if (players_hand4.empty()) {
 						split(players_hand);
-					} else {
-						hit(players_hand);
-						playerLogic(players_hand);
 					}
 					break;
 				case 3:
 					if (players_hand4.empty()) {
 						split(players_hand);
-					} else {
-						hit(players_hand);
-						playerLogic(players_hand);
 					}
 					break;
 				case 4:
 					if (players_hand4.empty()) {
 						split(players_hand);
-					} else {
-						hit(players_hand);
-						playerLogic(players_hand);
 					}
 					break;
 				case 5:
 					if (players_hand4.empty()) {
 						split(players_hand);
-					} else {
-						hit(players_hand);
-						playerLogic(players_hand);
 					}
 					break;
 				case 6:
 					if (players_hand4.empty()) {
 						split(players_hand);
-					} else {
-						hit(players_hand);
-						playerLogic(players_hand);
 					}
 					break;
 				case 7:
@@ -1448,6 +1458,8 @@ void playerLogic(vector<card> &players_hand) {
 				break;
 			case 10:
 				if (firstHand) {
+					// check if the players split hand is empty to ensure
+					// the player is not surrendering a split hand
 					if (players_hand2.empty()) {
 						surrender(players_hand);
 					} else {
@@ -1560,6 +1572,8 @@ void playerLogic(vector<card> &players_hand) {
 					break;
 				case 9:
 					if (firstHand) {
+						// check if the players split hand is empty to ensure
+						// the player is not surrendering a split hand
 						if (players_hand2.empty()) {
 							surrender(players_hand);
 						} else {
@@ -1573,6 +1587,8 @@ void playerLogic(vector<card> &players_hand) {
 					break;
 				case 10:
 					if (firstHand) {
+						// check if the players split hand is empty to ensure
+						// the player is not surrendering a split hand
 						if (players_hand2.empty()) {
 							surrender(players_hand);
 						} else {
@@ -1595,9 +1611,6 @@ void playerLogic(vector<card> &players_hand) {
 			if ((players_hand[0].r == 9) && (players_hand[1].r == 9)) {
 				switch(house_sum) {
 				case 1:
-					if (players_hand4.empty()) {
-						split(players_hand);
-					}
 					break;
 				case 2:
 					if (players_hand4.empty()) {
@@ -1653,6 +1666,7 @@ void playerLogic(vector<card> &players_hand) {
 			break;
 		}
 	} else {
+		// Soft hand player logic
 		switch(player_sum) {
 		case 12:
 			if (players_hand4.empty()) {
@@ -2055,6 +2069,7 @@ void houseLogic(vector<card> &house_hand) {
 	for(unsigned int i = 0; i < house_hand.size(); i++) {
 		house_sum += house_hand[i].r;
 	}
+	// Determine whether an ace will act as a one or eleven
 	for(unsigned int i = 0; i < house_hand.size(); i++) {
 		if (house_hand[i].r == 1) {
 			if ((house_sum + 10) < 22) {
@@ -2066,14 +2081,6 @@ void houseLogic(vector<card> &house_hand) {
 	// The combinations of all possible dealer hands and what the dealer must to
 	// for each hand
 	switch(house_sum) {
-	case 2:
-		hit(house_hand);
-		houseLogic(house_hand);
-		break;
-	case 3:
-		hit(house_hand);
-		houseLogic(house_hand);
-		break;
 	case 4:
 		hit(house_hand);
 		houseLogic(house_hand);
